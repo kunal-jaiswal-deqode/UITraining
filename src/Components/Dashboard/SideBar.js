2e06570c626f85d9b33cbf0eec80004aacb83663
Kunal Jaiswal <kjaiswal@techracers.io>
2020-11-30 02:17:30 +0530

    Updates

--- src/Components/Dashboard/SideBar.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./styles.module.css";
-const SideBar = () => {
-  return <div id="sideBar" className={styles.sideBar}></div>;
-};
-export default SideBar;
