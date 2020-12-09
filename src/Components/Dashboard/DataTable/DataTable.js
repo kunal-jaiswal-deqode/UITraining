f4e9ca1a8b64f2b7b5ba58b04f8f7043b4e3cb8d
Kunal Jaiswal <kjaiswal@techracers.io>
2020-12-10 01:36:47 +0530

    Add images in table

--- src/Components/Dashboard/DataTable/DataTable.js
+++ src/Components/Dashboard/DataTable/DataTable.js
@@ -4,6 +4,8 @@ import cx from "classnames";
 import { tableData } from "../constants";
 
 const DataTable = () => {
+  const imageUrl =
+    "https://cdn2.iconfinder.com/data/icons/avatars-2-7/128/1-512.png";
   return (
     <div className={styles.tableContainer}>
       <label className={styles.heading}>Projects</label>
@@ -22,7 +24,18 @@ const DataTable = () => {
               <td className={styles.projectName}>{tableEntry.projectName}</td>
               <td className={styles.startDate}>{tableEntry.startDate}</td>
               <td className={styles.dueDate}>{tableEntry.dueDate}</td>
-              <td className={styles.team}>{tableEntry.team}</td>
+              <td className={styles.team}>
+                <div className={styles.imageGroup}>
+                  {[...Array(tableEntry.team)].map((i) => (
+                    <img
+                      key={i}
+                      src={imageUrl}
+                      alt="personIcon"
+                      className={styles.image}
+                    />
+                  ))}
+                </div>
+              </td>
               <td className={styles.status}>{tableEntry.status}</td>
               <td className={styles.client}>{tableEntry.client}</td>
             </tr>
