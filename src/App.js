57ac3f217a39d913652ddc14cc7063ee68f8d77d
Kunal Jaiswal <kjaiswal@techracers.io>
2020-12-15 02:26:00 +0530

    Add small screen support

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DashBoard from "./Components/Dashboard/Dashboard";
 import Header from "./Components/Header/Header";
+import _debounce from "lodash.debounce";
 
 function App() {
-  const [showSideBar, setShowSideBar] = useState(true);
+  const [showSideBar, setShowSideBar] = useState(false);
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = _debounce(() => setWidth(window.innerWidth), 100);
+
+    window.addEventListener("resize", handleResize);
+    console.log(width);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
       <Header showSideBar={showSideBar} setShowSideBar={setShowSideBar} />
