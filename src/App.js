fef6285d1c0c81b01c1970669a5633101a5c8e05
Kunal Jaiswal <kjaiswal@techracers.io>
2020-12-15 02:42:14 +0530

    Fix sidebar issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import { useEffect, useState } from "react";
 import DashBoard from "./Components/Dashboard/Dashboard";
 import Header from "./Components/Header/Header";
-import _debounce from "lodash.debounce";
 
 function App() {
-  const [showSideBar, setShowSideBar] = useState(false);
-
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = _debounce(() => setWidth(window.innerWidth), 100);
-
-    window.addEventListener("resize", handleResize);
-    console.log(width);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  const [showSideBar, setShowSideBar] = useState(true);
 
   return (
     <div>
